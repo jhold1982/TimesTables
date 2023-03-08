aa82fa552e826e252ab3e95b49bbb818fde8f641
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-07 20:50:23 -0600

    first commit

---
+++ TimesTables/ActiveGameView.swift
@@ -0,0 +1,20 @@
+//
+//  ActiveGameView.swift
+//  TimesTables
+//
+//  Created by Justin Hold on 3/7/23.
+//
+
+import SwiftUI
+
+struct ActiveGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActiveGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActiveGameView()
+    }
+}
---
+++ TimesTables/GameConfigView.swift
@@ -0,0 +1,20 @@
+//
+//  GameConfigView.swift
+//  TimesTables
+//
+//  Created by Justin Hold on 3/7/23.
+//
+
+import SwiftUI
+
+struct GameConfigView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameConfigView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameConfigView()
+    }
+}
