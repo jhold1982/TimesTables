77be7be6fc737b3829c5d692e0d5a1010d6b647d
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-07 20:49:11 -0600

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Justin Hold on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
